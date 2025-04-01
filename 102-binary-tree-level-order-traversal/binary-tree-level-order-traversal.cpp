b953574efb6537dc64f7649909a754a2ed7d3310
Sarthak Awasthi <115409841+sarthakawsthi7@users.noreply.github.com>
2025-04-02 01:09:09 +0530

    Time: 4 ms (14.46%) | Memory: 17.3 MB (6.26%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -1,37 +1,39 @@
-// struct TreeNode {
-//     int val;
-//     TreeNode *left;
-//     TreeNode *right;
-//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-// };
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        if (root == NULL) return {};
-        
         vector<vector<int>> result;
+        if(root == NULL){
+            return result;
+        }
         queue<TreeNode*> q;
         q.push(root);
-
-        while (!q.empty()) {
-            int size = q.size();  
+        while(!q.empty()){
+            int size = q.size();
             vector<int> level;
-
-            for (int i = 0; i < size; i++) {
+            for(int i=0; i< size;i++){
                 TreeNode* front = q.front();
                 q.pop();
                 level.push_back(front->val);
-
-                if (front->left) q.push(front->left);
-                if (front->right) q.push(front->right);
+                if(front->left != NULL){
+                    q.push(front->left);
+                }
+                if(front->right != NULL){
+                    q.push(front -> right);
+                }
             }
-
             result.push_back(level);
         }
-
         return result;
     }
 };
