0d345ff3fd867fd9ec529fc965a02ece68cf2035
Sarthak Awasthi <115409841+sarthakawsthi7@users.noreply.github.com>
2025-04-05 01:35:52 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (67.50%) - LeetSync

--- 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
@@ -11,39 +11,65 @@
  */
 class Solution {
 public:
-    unordered_map<int,int> mp;
-    int maxD = 0;
-    TreeNode* LCA(TreeNode* root){
-   if(root == NULL || mp[root->val] == maxD){
-            return root;
-        }    
-        TreeNode* l = LCA(root->left);
-        TreeNode* r = LCA(root->right);
+//     unordered_map<int,int> mp;
+//     int maxD = 0;
+//     TreeNode* LCA(TreeNode* root){
+//    if(root == NULL || mp[root->val] == maxD){
+//             return root;
+//         }    
+//         TreeNode* l = LCA(root->left);
+//         TreeNode* r = LCA(root->right);
 
-        if(r != NULL && l != NULL){
-            return root;
-        }
-        if(l != NULL){
-            return l;
-        }
-        return r;
+//         if(r != NULL && l != NULL){
+//             return root;
+//         }
+//         if(l != NULL){
+//             return l;
+//         }
+//         return r;
     
-    }
-    void depth(TreeNode* root, int d){
-         if(root == NULL){
-            return;
-        }
-        maxD = max(maxD,d);
-        mp[root->val] = d;
-        depth(root->left,d+1);
-        depth(root->right,d+1);
-    }
-    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+//     }
+//     void depth(TreeNode* root, int d){
+//          if(root == NULL){
+//             return;
+//         }
+//         maxD = max(maxD,d);
+//         mp[root->val] = d;
+//         depth(root->left,d+1);
+//         depth(root->right,d+1);
+//     }
+//     TreeNode* lcaDeepestLeaves(TreeNode* root) {
 
-        if(root == NULL){
-            return NULL;
-        }
-        depth(root,0);
-        return LCA(root);  
+//         if(root == NULL){
+//             return NULL;
+//         }
+//         depth(root,0);
+//         return LCA(root); 
+
+
+// *******!!!!!!!Approach - 2 (by using Pairs {depth,TreeNode*})*******!!!!!!!
+pair<int, TreeNode*> solve(TreeNode* root){
+    if(root ==  NULL){
+        return{0,NULL};
+    }
+    auto l = solve(root->left);
+    auto r = solve(root->right);
+    if(l.first == r.first){
+        return {l.first+1, root};
+    }
+    else if(l.first > r.first){
+        return {l.first+1, l.second};
+    }
+    else{
+        return {r.first+1, r.second};
+    }
+}
+
+
+ TreeNode* lcaDeepestLeaves(TreeNode* root) {
+return solve(root).second;
+
+
+
     }
 };
