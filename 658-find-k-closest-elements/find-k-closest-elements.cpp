32bf8c21992d0ee41f96b17c5b08b96ea8185bc5
Sarthak Awasthi <115409841+sarthakawsthi7@users.noreply.github.com>
2025-07-12 15:02:44 +0530

    Time: 1 ms (62.73%) | Memory: 36.6 MB (45.96%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -1,19 +1,47 @@
 class Solution {
 public:
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int l=0, h = arr.size()-1;
+int lowerBound(vector<int>&arr, int x){
+    int low = 0;
+    int high = arr.size()-1;
+    int ans = high;
+    while(low <= high){
+        int mid = (low+high)/2;
+        if(arr[mid] >= x){
+            ans = mid;
+            high = mid-1;
+        }
+        else{
+            low = mid+1;
+        }
+       
+    }
+    return ans;
+}
+
+    vector<int> binarySearch_Method(vector<int>& arr, int k, int x){
         vector<int>ans;
-        while(h-l >= k){
-            if(x-arr[l] > arr[h] -x){
-                l++;
-            }
-            else{
-                h--;
+        int h= lowerBound(arr,x);
+        int l = h-1;
+        
+        while(k--){
+             if (l < 0) {
+                h++;
+            } else if (h >= arr.size()) {
+                l--;
+            } else if (x - arr[l] <= arr[h] - x) {
+                l--;
+            } else {
+                h++;
             }
         }
-        for(int i=l; i<=h; i++){
-            ans.push_back(arr[i]);
-        }
-        return ans;
+    
+    for(int i=l+1; i<h; i++){
+        ans.push_back(arr[i]);
+    }
+    return ans;
+}
+
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        return binarySearch_Method(arr,k,x);
     }
 };
